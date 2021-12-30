077953b8ae1aa743526b61771ceb3215720c4361
lukaswyver <lucas_sin5@hotmail.com>
2021-12-30 14:13:59 -0300

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -131,7 +131,6 @@ export default function Home() {
   const onChange = date => {
     setShow(Platform.OS === 'ios');
     setNewDate(date);
-    console.log(date);
   };
   return (
     <Background>
